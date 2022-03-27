ea31a7f3a88bdc47c27cde94c0ef77313cc903b8
irostub <irostub@gmail.com>
2022-03-27 22:13:49 +0900

    Add p31 to p33

---
+++ codefestival/31-40/031.js
@@ -0,0 +1,6 @@
+const a = "abc";
+const b = a.slice(1);
+const c = a.includes("c");
+
+//slice direct access and push attr O(n)
+//inclues compare one to one attr O(n)
---
+++ codefestival/31-40/033.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+const sourceFilename = __filename.substring(
+  __filename.lastIndexOf("/"),
+  __filename.lastIndexOf(".js")
+);
+const input = fs
+  .readFileSync(path.join(__dirname, `./testcase/${sourceFilename}`))
+  .toString()
+  .split()
+  .reverse()
+  .join();
+console.log(input);
